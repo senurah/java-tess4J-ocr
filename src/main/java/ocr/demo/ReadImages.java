eb0977ca7ea0ceb3af82f11b772731529aa81d7d
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-09-07 20:26:35 +0530

    Add cmd interactions

--- src/main/java/ocr/demo/ReadImages.java
+++ src/main/java/ocr/demo/ReadImages.java
@@ -7,15 +7,50 @@ import java.io.File;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReadImages {
     public static void main(String[] args) {
+
         ITesseract instance = new Tesseract();
-        instance.setDatapath("./tessdata"); // Set the tessdata folder path here
-        instance.setLanguage("eng"); // Set the language to 'eng'
+        instance.setDatapath("./tessdata");// Set the tessdata folder path here
+        //instance.setLanguage("sin");
+        // Set the language to 'eng' and 'sin' for English and Sinhala respectively
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select the language(Enter 1 or 2) :\n\t1)English\n\t2)Sinhala\nYour Option :");
+        while(true){
+            try{
+                int choice = scanner.nextInt();
+                if(choice == 1){
+                    instance.setLanguage("eng");
+                    break;
+                }else if(choice == 2){
+                    instance.setLanguage("sin");
+                    break;
+                }else{
+                    System.out.println("Invalid input. Please enter 1 or 2");
+                }
+
+            }
+            catch(Exception e){
+                System.out.println("Invalid input. Please enter 1 or 2");
+            }
+
+        }
+        scanner.nextLine();
+        String path = "";
+        while(true){
+            System.out.println("Enter the path of the image :");
+            path = scanner.next();
+            if(new File(path).exists() || path.equals("1")){
+                break;
+            }else{
+                System.out.println("Invalid path. Please enter a valid path.press(1) to exit.");
+            }
+        }
 
         try {
-            BufferedImage image = ImageIO.read(new File("src/images/testocr.png")); // Replace with actual path
+            BufferedImage image = ImageIO.read(new File(path)); // Replace with actual path
             String result = instance.doOCR(image);
             System.out.println(result);
         } catch (TesseractException | IOException e) {
