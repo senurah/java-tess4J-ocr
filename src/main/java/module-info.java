f1cfeac529d4a585663c1b7c9e23e5447a727a09
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-09-09 08:40:14 +0530

    add JavaFx GUI version1.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module java.ocr.tess4j {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires tess4j;
+    requires org.apache.pdfbox;
+    requires java.desktop;
+    requires java.logging;
+
+    requires org.kordamp.bootstrapfx.core;
+    requires org.slf4j;
+
+
+    exports ocr;
+
+}
+
